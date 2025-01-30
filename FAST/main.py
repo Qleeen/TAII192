53e1e6e4b013c04eea824bf96af3d14ace9d9ea0
Baruch O <146269567+Qleeen@users.noreply.github.com>
2025-01-30 16:59:50 -0600

    Practica 2 finalizada

--- FAST/main.py
+++ FAST/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app= FastAPI(
     title= 'Mi primerAPI 192',
     description= 'Paramétros de API',
     version= '1.0.1'
 )
+
+usuarios=[
+    {"id": 1,"nombre":"BaruchO","edad":20},
+    {"id": 2,"nombre":"Fernando","edad":22},
+    {"id": 3,"nombre":"Max","edad":20},
+    {"id": 4,"nombre":"Gera","edad":25}
+]
 
 #Endpoint home
 
@@ -19,8 +27,42 @@ def promedio():
 
 
 #Endpoint parametros Obligatorios
-@app.get('/usuario/{id}', tags=['parámetro Obligatorio'])
+@app.get('/usuario/{id}', tags=['Parámetro Obligatorio'])
 def consultaUsuario(id:int):
     #Conectamos a la BD
     #Consultamos
     return {'Se encontró el usuario':id}
+
+#Endpoint parametros Opcionales
+@app.get('/usuario/', tags=['Parámetro Opcional'])
+def consultaUsuario2(id:Optional[int]= None):
+    if id is not None:
+        for usu in usuarios:
+            if usu["id"] == id:
+                return {"mensaje": "Usuario encontrado", "usuario": usu}
+        return {"mensaje":f"Usuario no encontrado con el id : {id}"}
+    else:
+        return {"mensaje":"No se proporciono un ID"}
+    
+    
+#endpoint con varios parametro opcionales
+@app.get("/usuarios/", tags=["3 parámetros opcionales"])
+async def consulta_usuarios(
+    usuario_id: Optional[int] = None,
+    nombre: Optional[str] = None,
+    edad: Optional[int] = None
+):
+    resultados = []
+
+    for usuario in usuarios:
+        if (
+            (usuario_id is None or usuario["id"] == usuario_id) and
+            (nombre is None or usuario["nombre"].lower() == nombre.lower()) and
+            (edad is None or usuario["edad"] == edad)
+        ):
+            resultados.append(usuario)
+
+    if resultados:
+        return {"usuarios_encontrados": resultados}
+    else:
+        return {"mensaje": "No se encontraron usuarios que coincidan con los parámetros proporcionados."}
