225fc37174cc416fa035446e812db1ca5d985ffe
Baruch O <146269567+Qleeen@users.noreply.github.com>
2025-01-29 19:49:49 -0600

    Practica 2.1

--- FAST/main.py
+++ FAST/main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
 
-app= FastAPI()
+app= FastAPI(
+    title= 'Mi primerAPI 192',
+    description= 'Paramétros de API',
+    version= '1.0.1'
+)
 
 #Endpoint home
 
-@app.get('/')
+@app.get('/', tags=['Hola Mundo'])
 def home():
     return {'hello':'world FastAPI'}
+
+#Endpoint promedio
+@app.get('/promedio', tags=['Mi calificacion TAI'])
+def promedio():
+    return 10
+
+
+#Endpoint parametros Obligatorios
+@app.get('/usuario/{id}', tags=['parámetro Obligatorio'])
+def consultaUsuario(id:int):
+    #Conectamos a la BD
+    #Consultamos
+    return {'Se encontró el usuario':id}
